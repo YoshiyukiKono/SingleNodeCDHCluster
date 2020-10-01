05779eba5b4d348796c28411cff08d09c0042bd0
Yoshiyuki Kono <yoshiyuki.kono@gmail.com>
2020-10-01 12:58:59 +0900

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+sudo systemctl stop cloudera-scm-agent
+sudo systemctl stop cloudera-scm-server
+
 sudo mv /etc/hosts /etc/hosts.org
 sudo cp /etc/hosts.template /etc/hosts
 sudo sh -c "echo '`hostname -I`  `hostname`' >> /etc/hosts"
@@ -10,4 +13,7 @@ sudo sh -c "sed s/%SERVER_HOST%/`hostname`/ /etc/cloudera-scm-agent/config.ini.t
 sudo systemctl start cloudera-scm-agent
 sudo systemctl start cloudera-scm-server
 
-curl -X POST -u "admin:admin" -i -H "content-type:application/json" -d '{"restartOnlyStaleServices": false, "redepoyClientConfiguration": true}' http://localhost:7180/api/v11/clusters/OneNodeCluster/commands/restart
+curl -X POST -u "admin:admin" http://localhost:7180/api/v11/cm/service/commands/stop
+curl -X POST -u "admin:admin" http://localhost:7180/api/v11/cm/service/commands/start
+
+curl -X POST -u "admin:admin" -i -H "content-type:application/json" -d '{"restartOnlyStaleServices": false, "redeployClientConfiguration": true}' http://localhost:7180/api/v11/clusters/OneNodeCluster/commands/restart
